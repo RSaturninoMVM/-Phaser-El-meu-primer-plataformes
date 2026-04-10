1d0a761bc3251fdbccafb6e9f55af9b354680cc0
= <rsaturnino@institutmvm.cat>
2026-04-10 10:45:20 +0200

    second commit

--- el-meu-joc/src/main.js
+++ el-meu-joc/src/main.js
@@ -1,16 +1,8 @@
 import Phaser, { Physics } from "phaser";
 import './style.css';
-// import dudeImg from "assets/Sprite.jpeg"
+import dudeImg1 from "./assets/gemini1.png"
+import dudeImg2 from "./assets/gemini2.png"
 
-class EscenaProva extends Phaser.Scene {
-  constructor() { super({ key: 'EscenaProva' }); }
-
-  create() {
-    this.add.text(400, 300, 'PHASER ESTA VIU!!!', {
-      fontSize: '40px', color: '#00ff00', fontStyle: 'bold'
-    }).setOrigin(0.5);
-  }
-}
 
 const config = {
   type: Phaser.AUTO,
@@ -30,26 +22,67 @@ const config = {
   }
 };
 
-const game = new Phaser.Game(config);
 
-let cursors = this.input.keyboard
-  .createCursorKeys();
 
-if (cursors.left.isDown) {
-  player.setVelocityX(-160);
-  player.anims.play('left', true);
+function preload() {
+  this.load.spritesheet('dude1', 'dudeImg1', { frameWidth: 32, frameHeight: 48 });
 }
 
-if (cursors.up.isDown
-  && player.body.touching.down) {
-  player.setVelocityY(-330);
-}
+function create() {
+  this.add.image(400, 300, 'dude1');
 
-this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
 
-this.anims.create({
-  key: 'left',
-  frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-  frameRate: 10,
-  repeat: -1
+  let cursors = this.input.keyboard
+    .createCursorKeys();
+
+    this.physics
+  .add.collider(player, platforms);
+
+this.physics.add
+  .overlap(player,
+    stars,
+    collect,
+    null,
+    this
+  );
+
+stars = this.physics.add.group({
+  key: 'star',
+  repeat: 11,
+  setXY: { x: 12, y: 0, stepX: 70 }
 });
+
+stars.children.iterate((child) => {
+  child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+});
+
+let scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000000' });
+
+function collect (player, star) {
+  star.disableBody(true, true);
+  score += 10;
+  scoreText.setText('Score: ' + score);
+}
+}
+
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  };
+  if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  };
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  };
+};
+
+const game = new Phaser.Game(config);
