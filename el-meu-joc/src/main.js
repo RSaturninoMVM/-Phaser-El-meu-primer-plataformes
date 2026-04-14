d7046ff58614b71e593efeba578d904cbd8a3c8c
= <rsaturnino@institutmvm.cat>
2026-04-14 10:18:30 +0200

    version juego1

--- el-meu-joc/src/main.js
+++ el-meu-joc/src/main.js
@@ -1,7 +1,7 @@
 import Phaser, { Physics } from "phaser";
 import './style.css';
 import dudeImg from "./assets/sprite_nino.png";
-//import dudeImg2 from "";
+import dudeImg2 from "./assets/estrella.png";
 
 
 const config = {
@@ -11,7 +11,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 500 },
       debug: false
     }
   },
@@ -29,17 +29,20 @@ var score = 0;
 var scoreText;
 
 function preload() {
-  this.load.spritesheet('dude', 'dudeImg', { 
-    frameWidth: 32, frameHeight: 48 });
+  this.load.spritesheet('dude', dudeImg, { 
+    frameWidth: 122, frameHeight: 150
+  });
+
+  this.load.spritesheet('star', dudeImg2, { 
+    frameWidth: 150, frameHeight: 233
+  });
 }
 
 function create() {
   player = this.physics.add.sprite(100, 600, 'dude');
   player.setCollideWorldBounds(true);
-  stars = this.physics.add.sprite(500, 600, 'dude');
-  stars.setCollideWorldBounds(true);
-  cursors = this.input.keyboard.createCursorKeys();
 
+  
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 4 }),
@@ -50,37 +53,43 @@ function create() {
   stars = this.physics.add.group({
     key: 'star',
     repeat: 5,
-    setXY: { x: 200, y: 0, stepX: 70 }
+    setXY: { x: 300, y: 0, stepX: 90 }
   });
 
   stars.children.iterate((child) => {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.6));
+    child.setCollideWorldBounds(true);
+    child.setScale(0.6);
   });
 
-  let scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#ffffff' });
-}
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#ffffff' });
+  cursors = this.input.keyboard.createCursorKeys();
 
-  function collect (player, star) {
-    player.disableBody(true, true);
-    star.disableBody(true, true);
-    score += 10;
-    scoreText.setText('Score: ' + score);
+  this.physics.add.overlap(stars, player, collect, null, this);
 }
 
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
-  };
   if (cursors.right.isDown) {
     player.setVelocityX(160);
     player.anims.play('right', true);
+  } else if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(-0);
   };
-  if (cursors.up.isDown && player.body.touching.down) {
+  
+  if (cursors.up.isDown && (player.body.touching.down || player.body.blocked.down))  {
     player.setVelocityY(-330);
-    player.anims.play('up', true);
-  };
+  }
 };
+
+function collect (player, star) {
+  star.disableBody(true, true);
+  score += 11.166666666666666666666666666666666666667;
+  scoreText.setText('Score: ' + score);
+}
+
 
 
 const game = new Phaser.Game(config);
